0570036accfbb59ef1a07d6aff9c637cabcfbeb3
Nurutomo <tixbig@gmail.com>
2025-01-31 06:05:20 +0700

    inner workings

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
-export default function say(string) {
-    if (!string || typeof string !== 'string') throw new Error('Must be type of string')
-    console.log(`Hi, ${string}!`)
-    return true
-}
+import PluginManager from '@mynbites/plugin-manager'
+import { Connection } from './util/Connection.js'
+import { serialize } from './util/Message.js'
+// import Database from './util/Database.js'
 
-if (process.argv[1] === import.meta.filename) {
-  say('Hello World')
+/**
+ * @type {import('.').Plugin}
+ */
+export const Plugin = new PluginManager(import.meta.dirname)
+export const Conn = new Connection(process.argv[2] || 'default')
+// export const db = new (Function)()
+
+export default Plugin
+
+const isPair = process.env.NUMBER
+
+serialize()
+await Plugin.addPluginFolder('./plugins', true)
+await Conn.start({ printQRInTerminal: !isPair })
+// Conn.reload(false)
+if (isPair) {
+  console.log('Found env variable NUMBER with value', isPair)
+  await new Promise(resolve => setTimeout(resolve, 2000))
+  console.log('Your pairing code:', await Conn.getCode(process.env.NUMBER))
 }
+console.log('Hai')
